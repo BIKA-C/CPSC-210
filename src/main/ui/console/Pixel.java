6396775f73b0f7dbd3d3f95b11ddb5e7857c3092
William Chen <bikachen@students.cs.ubc.ca>
2022-02-05 00:50:51 -0800

    doc for Pixel

--- src/main/ui/console/Pixel.java
+++ src/main/ui/console/Pixel.java
@@ -1,11 +1,15 @@
 package ui.console;
 
+// Pixel represents a Pixel on the console screen
+// it has a character that can be printed and
+// TextAttribute which describes it's style
 public class Pixel {
     private final char character;
     private final TextAttribute attribute;
 
     public static final Pixel EMPTY_PIXEL = new Pixel(' ', TextAttribute.DEFAULT);
 
+    // EFFECTS: constructs a pixel with the given character and attribute
     public Pixel(char character, TextAttribute attribute) {
         this.character = character;
         this.attribute = attribute;
@@ -17,5 +21,11 @@ public class Pixel {
 
     public char getCharacter() {
         return character;
+    }
+
+    // REQUIRES: p != null
+    // EFFECTS: returns true if the given pixel has the same character and style
+    public boolean isSame(Pixel p) {
+        return p.character == character && attribute.isSame(p.attribute);
     }
 }
