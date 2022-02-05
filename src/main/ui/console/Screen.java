9063f7463d75b49623d07cf1d59ba93a8ab70843
William Chen <bikachen@students.cs.ubc.ca>
2022-02-05 00:51:11 -0800

    doc for Screen

--- src/main/ui/console/Screen.java
+++ src/main/ui/console/Screen.java
@@ -2,50 +2,80 @@ package ui.console;
 
 import model.utility.Coordinate;
 
+// Screen represents a terminal Screen with a width and height
+// this class represents a Screen Pixel as 2 characters to make
+// it looks like a square. So x is automatically multiplied by 2
+// assuming top right corner is (0, 0)
 public class Screen {
     private final int width;
     private final int height;
 
     private final Pixel[][][] buffers;
     private int currentBuffer;
+    private int previousBuffer;
 
+    // REQUIRES: width > 0 && height > 0
+    // EFFECTS: constructs an empty Screen with the given width and height
+    // and it clears the console
     public Screen(int width, int height) {
         this.width = width * 2;
         this.height = height;
         this.buffers = new Pixel[2][height][width * 2];
         this.currentBuffer = 0;
+        this.previousBuffer = 1;
         clear();
     }
 
+    // EFFECTS: clear all special text attribute
     public void clearAllAttribute() {
         System.out.print("\033[0m");
     }
 
+    // EFFECTS: clears the screen and all text attribute
+    // set the cursor to be visible
     public void clear() {
         System.out.print("\033[H\033[2J");
         setCursorVisibale();
     }
 
-    public void setCursor(Coordinate pos) {
-        System.out.print("\033[" + pos.getY() + ";" + pos.getX() * 2 + "H");
+    // REQUIRES: coord.getX() >= 0 && coord.getX() < getWidth() &&
+    // coord.getY() >= 0 && coord.getY() < getHeight() &&
+    // EFFECTS: set the cursor the the given Coordinate pos
+    public void setCursor(Coordinate coord) {
+        System.out.print("\033[" + coord.getY() + ";" + coord.getX() * 2 + "H");
     }
 
+    // REQUIRES: x >= 0 && x < getWidth() &&
+    // y >= 0 && y < getHeight() &&
+    // EFFECTS: set the cursor the the given x y
     public void setCursor(int x, int y) {
         System.out.print("\033[" + y + ";" + x * 2 + "H");
     }
 
+    // EFFECTS: put the cursor back to (0, 0)
     public void resetCursor() {
         System.out.print("\033[0;0H");
     }
 
+    // xxx // MODIFIES: this
+    // EFFECTS: put the current buffer to the screen
+    // after write or writePixel, render should be called
+    // to render the buffer to the screen.
+    // if render() is called twice without write or writePixel
+    // in between, you will have a empty screen
+    // this function has a little diff algorithm, so if it detects
+    // a pixel that is not changed(deep) between two render,
+    // this pixel will not be re-rendered (it will be skipped)
     public void render() {
-
-        resetCursor();
-
         Pixel pixel;
+        Pixel previousPixel;
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
                 pixel = buffers[currentBuffer][i][j];
+                previousPixel = buffers[previousBuffer][i][j];
+                if (previousPixel != null && (previousPixel == pixel || previousPixel.isSame(pixel))) {
+                    continue;
+                }
                 if (pixel == null) {
                     pixel = Pixel.EMPTY_PIXEL;
                 }
@@ -54,29 +84,46 @@ public class Screen {
         }
 
         this.currentBuffer = currentBuffer == 0 ? 1 : 0;
+        this.previousBuffer = previousBuffer == 0 ? 1 : 0;
         clearCurrentBuffer();
     }
 
-    public void setCursorInvisibale() {
+    // EFFECTS: set the cursor to be invisible
+    public void setCursorInvisible() {
         System.out.print("\033[?25l");
     }
 
+    // EFFECTS: set the cursor to be visible
     public void setCursorVisibale() {
         System.out.print("\033[?25h");
     }
 
-    public void setCursorVisiblity(boolean visiblity) {
-        if (visiblity) {
+    // EFFECTS: set the cursor to be visible/invisible by the
+    // boolean visibility
+    public void setCursorVisiblity(boolean visibility) {
+        if (visibility) {
             setCursorVisibale();
         } else {
-            setCursorInvisibale();
+            setCursorInvisible();
         }
     }
 
-    public void writePixel(Pixel p, Coordinate pos, boolean wide) {
-        writePixel(p, pos.getX(), pos.getY(), wide);
+    // REQUIRES: coord.getX() >= 0 && coord.getX() < getWidth() &&
+    // coord.getY() >= 0 && coord.getY() < getHeight() &&
+    // xxx MODIFIES: this?
+    // EFFECTS: write Pixel p on to the background buffer at the given position.
+    // If wide it will draw it twice, else an EMPTY_PIXEL will be placed
+    // requires render() to render the buffer to the screen
+    public void writePixel(Pixel p, Coordinate coord, boolean wide) {
+        writePixel(p, coord.getX(), coord.getY(), wide);
     }
 
+    // REQUIRES: x >= 0 && x < getWidth() &&
+    // y >= 0 && y < getHeight() &&
+    // xxx MODIFIES: this?
+    // EFFECTS: write Pixel p on to the background buffer at the given position.
+    // If wide it will draw it twice, else an EMPTY_PIXEL will be placed
+    // requires render() to render the buffer to the screen
     public void writePixel(Pixel p, int x, int y, boolean wide) {
         buffers[currentBuffer][y][x * 2] = p;
         if (wide) {
@@ -86,28 +133,51 @@ public class Screen {
         }
     }
 
-    public void write(String s, Coordinate pos, TextAttribute style) {
-        write(s, pos.getX(), pos.getY(), style);
+    // REQUIRES: coord.getX() >= 0 && coord.getX() < getWidth() &&
+    // coord.getY() >= 0 && coord.getY() < getHeight() &&
+    // s.length() + coord.getX() < getWidth()
+    // xxx MODIFIES: this?
+    // EFFECTS: write s onto the buffer with the given position
+    // and style. If wide and s.length() == 1, it will draw the string twice
+    // if not wide and s.length() == 1, an EMPTY_PIXEL will be added
+    // requires render() to render the buffer to the screen
+    public void write(String s, Coordinate coord, TextAttribute style, boolean wide) {
+        write(s, coord.getX(), coord.getY(), style, wide);
     }
 
-    public void write(String s, int x, int y, TextAttribute style) {
+    // REQUIRES: x >= 0 && x < getWidth() &&
+    // y >= 0 && y < getHeight() &&
+    // s.length() + x < getWidth()
+    // xxx MODIFIES: this?
+    // EFFECTS: write s onto the buffer with the given position
+    // and style. If wide and s.length() == 1, it will draw the string twice
+    // if not wide and s.length() == 1, an EMPTY_PIXEL will be added
+    // requires render() to render the buffer to the screen
+    public void write(String s, int x, int y, TextAttribute style, boolean wide) {
         Pixel pixel;
         for (int i = 0; i < s.length(); i++) {
             pixel = new Pixel(s.charAt(i), style);
             buffers[currentBuffer][y][x * 2 + i] = pixel;
         }
+        if (wide && s.length() == 1) {
+            buffers[currentBuffer][y][x * 2 + 1] = buffers[currentBuffer][y][x * 2];
+        } else if (s.length() == 1) {
+            buffers[currentBuffer][y][x * 2 + 1] = Pixel.EMPTY_PIXEL;
+        }
     }
 
-    // REQUIRES: x >= 0 && x <= getWidth() && y>= 0 && y <= getHeight()
-    // && s.length() + x <= getWidth()
+    // REQUIRES: x >= 0 && x < getWidth() &&
+    // y >= 0 && y < getHeight() &&
+    // s.length() + x < getWidth()
     // EFFECTS: immediately put s onto the screen with the given position
-    // and style. If wide, it will draw it twice
+    // and style. If wide and s.length() == 1, it will draw the string twice
+    // if not wide and s.length() == 1, an empty space will be added
     public void draw(String s, int x, int y, TextAttribute style, boolean wide) {
         setCursor(x + 1, y + 1);
 
         applyStyle(style);
         System.out.print(s);
-        if (wide) {
+        if (wide && s.length() == 1) {
             System.out.print(s);
         } else if (s.length() == 1) {
             System.out.print(" ");
@@ -116,19 +186,28 @@ public class Screen {
         clearAllAttribute();
     }
 
-    // REQUIRES: coord.getX() >= 0 && coord.getX() <= getWidth() &&
-    // coord.getY()>= 0 && coord.getY() <= getHeight()
-    // && s.length() + x <= getWidth()
+    // REQUIRES: coord.getX() >= 0 && coord.getX() < getWidth() &&
+    // coord.getY() >= 0 && coord.getY() < getHeight() &&
+    // s.length() + coord.getX() < getWidth()
     // EFFECTS: immediately put s onto the screen with the given position
-    // and style. If wide, it will draw it twice
+    // and style. If wide and s.length() == 1, it will draw the string twice
+    // if not wide and s.length() == 1, an empty space will be added
     public void draw(String s, Coordinate coord, TextAttribute style, boolean wide) {
         draw(s, coord.getX(), coord.getY(), style, wide);
     }
 
+    // REQUIRES: x >= 0 && x < getWidth() &&
+    // y >= 0 && y < getHeight() &&
+    // EFFECTS: immediately put Pixel p on to the screen with the given position.
+    // If wide it will draw it twice, else an EMPTY_PIXEL will be placed
     public void drawPixel(Pixel p, int x, int y, boolean wide) {
         draw(Character.toString(p.getCharacter()), x, y, p.getAttribute(), wide);
     }
 
+    // REQUIRES: coord.getX() >= 0 && coord.getX() < getWidth() &&
+    // coord.getY() >= 0 && coord.getY() < getHeight() &&
+    // EFFECTS: immediately put Pixel p on to the screen with the given position.
+    // If wide it will draw it twice, else an EMPTY_PIXEL will be placed
     public void drawPixel(Pixel p, Coordinate coord, boolean wide) {
         drawPixel(p, coord.getX(), coord.getY(), wide);
     }
