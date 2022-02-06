fc25eae7d62b9915327cbbe4acd345f2b083162d
William Chen <bikachen@students.cs.ubc.ca>
2022-02-06 01:47:24 -0800

    support for new items

--- src/main/model/Game.java
+++ src/main/model/Game.java
@@ -3,8 +3,12 @@ package model;
 import java.util.ArrayList;
 import java.util.Random;
 
+import model.item.Breaker;
 import model.item.Coin;
+import model.item.Hint;
 import model.item.Item;
+import model.item.Items;
+import model.item.Skip;
 import model.maze.Maze;
 import model.player.Player;
 import model.utility.Coordinate;
@@ -17,7 +21,11 @@ public class Game {
     private ArrayList<Coordinate> itemPosition;
     private ArrayList<Item> items;
 
+    private String itemMessage;
+
     private Random random;
+
+    private static final int NUM_OF_ITEM = 12;
 
     // EFFECTS: Constructs a new game object with a new player at 0,0
     // and a random maze
@@ -72,15 +80,38 @@ public class Game {
         return player;
     }
 
-    private void generateCoins() {
-        for (int i = 0; i < 3; i++) {
-            items.add(new Coin(random.nextInt(20) + 1));
+    public String getItemMessage() {
+        return itemMessage;
+    }
+
+    public void setItemMessage(String itemMessage) {
+        this.itemMessage = itemMessage;
+    }
+
+    private void generateItems() {
+        int index;
+        for (int i = 0; i < NUM_OF_ITEM; i++) {
+            index = random.nextInt(Items.values().length);
+            Items type = Items.values()[index];
+            if (type == Items.coin) {
+                items.add(new Coin(random.nextInt(20) + 1));
+            } else if (type == Items.breaker) {
+                int d = random.nextInt(5) + 1;
+                items.add(new Breaker(d));
+            } else if (type == Items.skip) {
+                items.add(new Skip());
+            } else if (type == Items.hint) {
+                items.add(new Hint());
+            } else {
+                items.add(new Coin(random.nextInt(20) + 1));
+            }
         }
     }
 
     private void init() {
         player.setPosition(maze.getStart());
         itemPosition = new ArrayList<>();
+        itemMessage = "New Maze !!!";
 
         int index;
         do {
@@ -89,9 +120,9 @@ public class Game {
             if (!itemPosition.contains(road)) {
                 itemPosition.add(road);
             }
-        } while (itemPosition.size() < 5);
+        } while (itemPosition.size() < NUM_OF_ITEM);
 
         items = new ArrayList<>();
-        generateCoins();
+        generateItems();
     }
 }
