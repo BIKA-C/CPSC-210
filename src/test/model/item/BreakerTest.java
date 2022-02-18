1c23588473afd38750888514a8ac7b4d8cd0662c
William Chen <bikachen@students.cs.ubc.ca>
2022-02-17 16:37:38 -0800

    delete unused import

--- src/test/model/item/BreakerTest.java
+++ src/test/model/item/BreakerTest.java
@@ -2,7 +2,6 @@ package model.item;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
