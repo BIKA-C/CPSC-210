bba4e158360e41acbac2999c5cb6ffd8482847c8
William Chen <bikachen@students.cs.ubc.ca>
2022-03-04 04:16:54 -0800

    bug fix

--- src/test/model/utility/menu/MenuTest.java
+++ src/test/model/utility/menu/MenuTest.java
@@ -1,8 +1,8 @@
 package model.utility.menu;
 
-import static org.junit.Assert.assertSame;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
